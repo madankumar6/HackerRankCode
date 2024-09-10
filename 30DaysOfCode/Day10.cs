75418cc9852618d8c861e185320f2a7074f6224a
madankumar6 <madankumar6@gmail.com>
2024-09-11 00:50:24 +0530

    Added initial code for 30 days of code contest of HackerRank

---
+++ 30DaysOfCode/Day10.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30DaysOfCode
+{
+    internal class Day10 : DayProgram
+    {
+        public override void RunProgram()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 30DaysOfCode/Program.cs
@@ -0,0 +1,38 @@
+﻿
+namespace _30DaysOfCode
+{
+
+    public abstract class DayProgram
+    {
+        public abstract void RunProgram();
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of the day of the program you want to run (1-30, strictly!)!");
+            int day = int.Parse(Console.ReadLine() ?? "1"); 
+
+            DayProgram program;
+
+            try
+            {
+                program = GetProgramClass(day); // new Day10();
+                program.RunProgram();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message); return;
+            }
+        }
+
+        private static DayProgram GetProgramClass(int day)
+        {
+            return day switch
+            {
+                10 => new Day10(),
+                _ => throw new InvalidDataException("This day doesn't exist in this contest")
+            };
+        }
+    }
+}
